925cb0382e42399785ccf6a7f1831143fbd6d04a
Garrett J Harty <gharty@live.com>
2021-11-02 16:29:39 -0500

    setup merge sort algorithm

--- src/SortingVisualizer/Algorithms/mergeSort.jsx
+++ src/SortingVisualizer/Algorithms/mergeSort.jsx
@@ -1,6 +1,37 @@
-function mergeSort(array) {
-    console.log('Initializing merge sort');
+function mergeArrays(left, right) {
+    const mergedArray = [];
+
+    while (left.length && right.length) {
+        mergedArray.push(left[0] > b[0] ? right.shift() : left.shift());
+    }
+
+    while (left.length) {
+        mergedArray.push(left.shift());
+    }
+    while (right.length) {
+        mergedArray.push(right.shift());
+    }
+
+    return mergedArray;
 }
+
+
+function mergeSort(arrayToSort) {
+    console.log('Initializing merge sort');
+
+    if (arrayToSort.length < 2) {
+        return arrayToSort
+    }
+
+    const middle = Math.floor(arrayToSort.length / 2);
+    const arrayLeft = arrayToSort.slice(0, middle);
+    const arrayRight = arrayToSort.slice(middle, arrayToSort.length);
+    const sortedLeft = mergeSort(arrayLeft);
+    const sortedRight = mergeSort(arrayRight);
+
+    return mergeArrays(sortedLeft, sortedRight);
+}
+
 
 module.exports = mergeSort;
 
